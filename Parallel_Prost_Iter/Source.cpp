37dd38f18fb9346812ff5c6f617c446df1e86089
Vladislav Nesterov <nesterovvlad100@gmail.com>
2022-07-04 19:02:10 +0300

    final

--- Parallel_Prost_Iter/Source.cpp
+++ Parallel_Prost_Iter/Source.cpp
@@ -13,17 +13,18 @@ double masA[n][n] = { { 0.0, -0.1, -0.1 },
 
 double masB[n] = { 1.2, 1.3, 1.4 };
 
-long double x[n] = { 0.0, 0.0, 0.0}, x0[n] = { 0.0, 0.0, 0.0 }, max1[n] = { 0.0, 0.0, 0.0 };
+long double x[n], x0[n], max1[n];
 int counter = 0, counter1[1] = { 0 };
 double rbufA[n * n];
 double rbuf[n * n];
 double rbufB[n * n];
 double rbufC[n * n];
+double rbufT[n * n];
 
 
 int main() {
     MPI_Init(NULL, NULL);
-    double starttime, endtime;
+    double starttime, endtime,all_time[n];
     starttime = MPI_Wtime();
 
     int rank, size;
@@ -37,81 +38,49 @@ int main() {
         x0[i] = masB[i];
     }
 
-    //MPI_Scatter(masB, 1, MPI_DOUBLE, rbufA, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
     do
     {
-
         MPI_Bcast(masA, n * n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Bcast(masB, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         MPI_Bcast(rbufC, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         for (size_t i = 0; i < n; i++)
         {
             x0[i] = rbufC[i];
-            //cout << "rank = " << rank << " rbufC prisvaemoviy = " << rbufC[rank] << endl;
         }
        
         x[rank] = 0.0;
         for (int j = 0; j < n; j++) {
             x[rank] += masA[rank][j] * x0[j];
-            cout << "rank = " << rank << "  do + bufa x0  = " << masA[rank][j] * x0[j] << endl;
         };
-        //cout << "rank = " << rank << "  do + bufa x  = " << x[rank] << endl;
-        //cout << "rank = " << rank << "  do + bufa x0  = " << x0[rank] << endl;
-        //cout << "rank = " << rank << "  do + bufa x - x0  = " << x[rank] - x0[rank] << endl;
-        //cout << "rank = " << rank << "  do + bufa |x - x0|  = " << fabs(x[rank] - x0[rank]) << endl;
 
         x[rank] += masB[rank];
 
-        //cout << "rank = " << rank << "  posle + bufa x  = " << x[rank] << endl;
-        //cout << "rank = " << rank << "  posle + bufa x0  = " << x0[rank] << endl;
-        //cout << "rank = " << rank << "  posle + bufa x - x0  = " << x[rank] - x0[rank] << endl;
-        //cout << "rank = " << rank << "  posle + bufa pered obnuleniem max1 |x - x0|  = " << fabs(x[rank] - x0[rank]) << endl;
         max1[rank] = 0.0;
 
-
         if (max1[rank] < fabs(x[rank] - x0[rank])) {
-            max1[rank] = fabs(x[rank] - x0[rank]);
-            cout << "rank = " << rank << " max1  = " << max1[rank] << endl;
+            max1[rank] = fabs(x[rank] - x0[rank]);  
         }
         x0[rank] = x[rank];
 
-
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Gather(&x0[rank], 1, MPI_DOUBLE, rbufC, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-     
 
-
-        // cout << "rank = " << rank << " x  = " << x[rank] << endl;
-        cout << "rank = " << rank << " rbufC  = " << rbufC[rank] << endl;
-        // cout << "rank = " << rank << " x - x0  = " << x[rank] - x0[rank] << endl;
-        // cout << "rank = " << rank << " |x - x0|  = " << fabs(x[rank] - x0[rank]) << endl;
-        // cout << "rank = " << rank << " max1 1 = " << max1[0] << endl;
         counter++;
-        cout << " counter Iter = " << counter << endl;
-        // cout << "rank = " << rank << "max1 = " << max1[rank] << endl;
-        MPI_Barrier(MPI_COMM_WORLD);
 
+        MPI_Barrier(MPI_COMM_WORLD);
         MPI_Gather(&max1[rank], 1, MPI_DOUBLE, rbufB, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         counter1[0] = 0;
         if (rank == 0)
         {
-            //cout << " counter Iter = " << counter << endl;
-            cout << "rank = " << rank << " max1 1 = " << rbufC[0] << endl;
-            cout << "rank = " << rank << " max1 2 = " << rbufC[1] << endl;
-            cout << "rank = " << rank << " max1 3 = " << rbufC[2] << endl;
-            //cout << "rank = " << rank << " max1 4 = " << rbufB[3] << endl;
             for (int i = 0; i < n; i++) {
                 if (rbufB[i] < eps)
                 {
                     counter1[0]++;
-                    cout << "rank = " << rank << " counter1 = " << counter1[0] << endl;
-                    //cout << "rank = " << rank << " rbufB = " << rbufB[i] << endl;
-                    fflush(stdout);
                 }
             }
         }
+        fflush(stdout);
         MPI_Barrier(MPI_COMM_WORLD);
         MPI_Bcast(counter1, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     } while (counter1[0] != n);
@@ -128,7 +97,13 @@ int main() {
     }
 
     endtime = MPI_Wtime();
-    printf("vipolnenie zanyalo %f seconds\n", endtime - starttime);
+    all_time[rank] = endtime - starttime;
+    MPI_Gather(&all_time[rank], 1, MPI_DOUBLE, rbufT, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if (rank==0)
+    {
+        rbufT[0] += rbufT[1] + rbufT[2];
+        printf("vipolnenie zanyalo %f seconds\n", rbufT[0]);
+    }
     MPI_Finalize();
     return 1;
 }
